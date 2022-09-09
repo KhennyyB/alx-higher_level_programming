740846101e4eee003f0db840faccaa52c33ad6be
KhennyyB <khennyphresh@gmail.com>
2022-09-09 09:20:16 -0700

    fix error

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -17,8 +17,8 @@ def list_all():
                          db=db_name, port=port)
     cur = db.cursor()
     cur.execute('SELECT cities.id, cities.name, states.name FROM cities' +
-            ' INNER JOIN states ON cities.state_id = states.id' +
-            ' ORDER BY cities.id ASC;')
+                ' INNER JOIN states ON cities.state_id = states.id' +
+                ' ORDER BY cities.id ASC;')
     result = cur.fetchall()
     cur.close()
     db.close()
